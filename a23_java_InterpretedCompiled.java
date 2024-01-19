908f9e9c4245aadf7a9bc04ae5b6aff03deddc44
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-01-19 10:35:35 +0530

    Adding Notes

---
+++ a23_java_InterpretedCompiled.java
@@ -0,0 +1,23 @@
+package Java;
+
+public class a23_java_InterpretedCompiled {
+    public static void main(String[] args) {
+        /*
+        when language has a line by line execution then it is an interpreted language
+
+        when language is compiled into exe file, and then runs it is compiled language
+
+        JAVA is a hybrid language it compiles java file into bytecode(class file) and then
+         can be used by java interpreter (JVM)
+
+        This bytecode can be interpreted at any platform like window, linux, mac etc
+         Hence java is platform independent (write once run Everywhere)
+
+         */
+        /*
+                                        Without Packages
+         First make a name.java file and then compile it on terminal from command (javac name.java)
+         then run it through interpreting using (java name.class)
+         */
+    }
+}
