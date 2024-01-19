d9b0dff6373f3307fde662982a7abdaeaf15c6a8
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-01-19 10:47:10 +0530

    Adding Notes

--- a24_java_Packages.java
+++ a24_java_Packages.java
@@ -1,2 +1,26 @@
-package Java;public class a24_java_Packages {
+package Java;
+
+import java.util.Scanner;
+
+public class a24_java_Packages {
+    public static void main(String[] args) {
+        /*
+        A package is used to group related classes
+        packages is used to avoid name conflicts
+
+        there are two type of packages
+        user defined / custom packages -> import java.util.Scanner
+        built in / java api packages -> package Java in this file
+         */
+        /*
+        Java package use
+         */
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+
+        /*
+        How to create your own package and use it
+        -- In Vs code --
+         */
+    }
 }
