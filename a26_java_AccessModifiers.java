ee3ab76c733edb78cf6305b5defdd7018eecaa69
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-01-20 17:28:47 +0530

    Access Modifiers

--- a26_java_AccessModifiers.java
+++ a26_java_AccessModifiers.java
@@ -1,2 +1,29 @@
-package Java;public class a26_java_AccessModifiers {
+package Java;
+class Parent26{
+    private void meth1(){
+        System.out.println("I can be used only inside class");
+    }
+    void meth2(){
+        System.out.println("I can be used inside class and in this packages(this code)");
+    }
+
+    protected void meth4(){
+        System.out.println("I can be used by class , packages and subclass(of another package) ");
+    }
+    public void meth3(){
+        System.out.println("I CAN BE USED ANYWHERE");
+    }
+}
+public class a26_java_AccessModifiers {
+    public static void main(String[] args) {
+        /*
+        Access modifiers determine whethere other class can use a perticular field or.
+         invoke a method or not.
+         This is of four type.
+         1.public.(can be used everywhere)
+         2.private.( can be used by class only)
+         3.default.(no modifier)(can be used by class and packages)
+         4.protected.( can be used by class,package,subclass)
+         */
+    }
 }
