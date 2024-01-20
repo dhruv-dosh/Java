c121b2dc5a283f669f6c7c04af5d5af005bf00bc
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-01-20 17:26:27 +0530

    Access Modifiers

---
+++ a26_java_AccessModifiers.java
@@ -0,0 +1,2 @@
+package Java;public class a26_java_AccessModifiers {
+}
---
+++ a27_java_Multithreding.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class a27_java_Multithreding {
+    public static void main(String[] args) {
+
+    }
+}
