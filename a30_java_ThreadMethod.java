9b899df49f750403c3191600ec47b897795029c9
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-01-21 17:08:04 +0530

    Thread

---
+++ a30_java_ThreadMethod.java
@@ -0,0 +1,40 @@
+package Java;
+class Mythr30 extends Thread{
+    public void run(){
+        int i =0;
+        while (i<100){
+            System.out.println("thread is running");
+            try {
+                Thread.sleep(1);//Delay in implementing thread
+            }
+            catch (Exception e){
+                System.out.println("Error");
+            }
+            i++;
+        }
+
+    }
+}
+class Mythr31 extends Thread{
+    public void run(){
+        int i =0;
+        while (i<100){
+        System.out.println("thread 2 is running -------------------");
+        i++;
+        }
+    }
+}
+public class a30_java_ThreadMethod {
+    public static void main(String[] args) {
+        Mythr30 t1 = new Mythr30();
+        Mythr31 t2 = new Mythr31();
+        t1.start();
+//        try {
+//           t1.join();// obj t1 will run first and then t2 will join
+//        }
+//        catch (Exception e){
+//            System.out.println("Error");
+//        }
+        t2.start();
+    }
+}
