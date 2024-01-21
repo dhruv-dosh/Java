6549c818ec49c5d25086799999de8609810dce15
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-01-21 22:39:03 +0530

    Error & Exception

--- a31_java_ErrorAndException.java
+++ a31_java_ErrorAndException.java
@@ -1,2 +1,13 @@
-package Java;public class a31_java_ErrorAndException {
+package Java;
+
+public class a31_java_ErrorAndException {
+    public static void main(String[] args) {
+        /*
+        There are three types of error
+        1.Syntax Error. > when compiler see any problem in our code it shows syntax error
+        2.logical Error. > Also known as bug occurs when program compiles and runs but does the wrong
+                           thing EG :- message delivered wrongly,
+        3.Runtime Error. -> Also called Exception
+         */
+    }
 }
