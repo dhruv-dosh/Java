04658f538c04688f7fc18b889b5a16fa9eee7f13
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-08-11 12:54:55 +0530

    Adding Notes

--- a02_java_String.java
+++ a02_java_String.java
@@ -1,20 +1,23 @@
 package Java;
 public class a02_java_String {
     public static void main(){
-        String d = "  dhruv  ";
+        //String is a java class which is used as datatype
+        String a = new String("dhruv");
+        String b = new String();
+        String d = "  dhruv  "; //but java provide us a feature to write string like this
+
+
+        //HERE ARE SOME FUNCTIONS TO MANIPULATE STRINGS
         System.out.println(d.length());
         d.toUpperCase();
-        System.out.println(d);
         d.toLowerCase();
-        System.out.println(d);
         d.trim();//Cut extra spaces
-        System.out.println(d);
         System.out.println(d.substring(2,4)); //begin and end index gives substring
         d.replace('d','l');//replace all char d with l
         d.startsWith("dh");//return boolean val
         d.endsWith("uv");//return boolean val
-        d.charAt(2);
-        d.indexOf('r');
+        d.charAt(2); //return char at index
+        d.indexOf('r'); //return index of char in String
         d.indexOf('u',2);//RETURN INDEX OF U AFTER INDEX 2
         d.lastIndexOf('r');//return where last r is present
         d.equals("dhruv");//return true if given string is equal
