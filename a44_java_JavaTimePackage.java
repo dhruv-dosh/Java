25461b5403a0e20d140baf16c4e265e8b355d4d6
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-08-30 15:16:24 +0530

    Adding Notes

--- a44_java_JavaTimePackage.java
+++ a44_java_JavaTimePackage.java
@@ -7,19 +7,19 @@ import java.time.format.DateTimeFormatter;
 
 public class a44_java_JavaTimePackage {
     public static void main(String[] args) {
-        //IN CLASS DATES A IMMUTABLE WHILE IN CALENDER THEY ARE MUTABLE
+        //IN CLASS DATES ARE IMMUTABLE WHILE IN CALENDAR THEY ARE MUTABLE
         LocalDate d = LocalDate.now();
         System.out.println(d);
         System.out.println(LocalTime.now());
         System.out.println(LocalDateTime.now());
 
 
-        //DATE TIME FORMATER
+        //DATE TIME FORMATTER
         LocalDateTime dt = LocalDateTime.now();
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-mm-yyyy");// you can get info of d,m,y,etc. from java doc
+        //DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-mm-yyyy");// you can get info of d,m,y,etc. from java doc
         DateTimeFormatter df2 = DateTimeFormatter.ISO_LOCAL_DATE;
-        String mydate = dt.format(df2);
-        System.out.println(mydate);
+        String myDate = dt.format(df2);
+        System.out.println(myDate);
 
     }
 }
