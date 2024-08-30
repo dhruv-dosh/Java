a917efe37953b5719aed812e37257b2e793e4ed0
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2024-08-30 15:16:01 +0530

    Adding Notes

--- a42_java_CalendarClass.java
+++ a42_java_CalendarClass.java
@@ -6,11 +6,11 @@ import java.util.TimeZone;
 public class a42_java_CalendarClass {
     public static void main(String[] args) {
         //Calendar is abstract class
-        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("Australia/Sydney"));
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("Asia/Kolkata"));
         System.out.println(c.getCalendarType());
         System.out.println(c.getTimeZone());
         System.out.println(c.getCalendarType());
-        System.out.println(c.get(Calendar.DATE)+":"+(c.get(Calendar.MONTH))+1+":"+(c.get(Calendar.YEAR)));
+        System.out.println(c.get(Calendar.DATE)+":"+(c.get(Calendar.MONTH)+1)+":"+(c.get(Calendar.YEAR)));
         System.out.println(c.get(Calendar.DAY_OF_MONTH));
         System.out.println(c.get(Calendar.DAY_OF_WEEK));
         System.out.println(c.get(Calendar.HOUR)+":"+c.get(Calendar.MINUTE)+":"+(c.get(Calendar.SECOND)));
