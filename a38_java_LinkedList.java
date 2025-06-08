d5445bc23a7e108ec3dda131073b544713ccd285
Dhruv Doshi <118899158+dhruv-dosh@users.noreply.github.com>
2025-06-08 15:39:22 +0530

    Update a38_java_LinkedList.java

--- a38_java_LinkedList.java
+++ a38_java_LinkedList.java
@@ -4,6 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class a38_java_LinkedList {
+    class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+   }
+
+        
+    
+
+
     public static void main(String[] args){
         List<Integer> ls = new LinkedList<>();
         ls.add(1);
@@ -11,5 +30,9 @@ public class a38_java_LinkedList {
         ls.add(3);
         System.out.println(ls.get(1));
         System.out.println(ls.size());
+
+        int[] arr = {2, 5, 6, 8};
+        Node y = new Node(arr[3]); // arr[3] = 8
+        System.out.print(y.data);  // should print 8
     }
 }
